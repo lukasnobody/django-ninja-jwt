2f28cbeb1a76ffe79fcca61dea5f0c825ce9b25d
Lukas Nobody <128538829+lukasnobody@users.noreply.github.com>
2025-09-21 15:51:54 +0300

    Create 0013_alter_outstandingtoken_options_and_more.py

---
+++ ninja_jwt/token_blacklist/migrations/0013_alter_outstandingtoken_options_and_more.py
@@ -0,0 +1,31 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        ('token_blacklist', '0012_alter_outstandingtoken_user'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='outstandingtoken',
+            options={'ordering': ('user_object_id',)},
+        ),
+        migrations.RemoveField(
+            model_name='outstandingtoken',
+            name='user',
+        ),
+        migrations.AddField(
+            model_name='outstandingtoken',
+            name='user_content_type',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='contenttypes.contenttype'),
+        ),
+        migrations.AddField(
+            model_name='outstandingtoken',
+            name='user_object_id',
+            field=models.PositiveIntegerField(blank=True, null=True),
+        ),
+    ]
